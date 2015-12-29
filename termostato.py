5b24744b3ec4891121f167891413e68b3e27d6b0
feranto63 <f.antonelli@tin.it>
2015-12-29 16:15:43 +0000

    aggiustati i commenti

--- termostato.py
+++ termostato.py
@@ -1,8 +1,10 @@
-//prima versione del termostato su pi
-// flowchart
-// ogni 5 minuti leggi la temperatura e lo stato della caldaia
-// memorizza i dati sul DB
-// grafica lo storico orario
-// richiedi in input la temperatura desiderata
-// se Tdes > Tact then turnon caldaia
-// loop
+#prima versione del termostato su pi
+#flowchart
+#ogni 5 minuti leggi la temperatura e lo stato della caldaia
+#memorizza i dati sul DB
+#grafica lo storico orario
+#richiedi in input la temperatura desiderata
+Tdes=raw_input("temperatura desiderata = ")
+print "Tdes=",Tdes
+#se Tdes > Tact then turnon caldaia
+#loop
