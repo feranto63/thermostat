d37c18a1209420e39b0c9d48e5c00e159adb2eac
feranto63 <feranto63@gmail.com>
2015-12-29 17:05:08 +0100

    Create termostato.py

---
+++ termostato.py
@@ -0,0 +1,8 @@
+//prima versione del termostato su pi
+// flowchart
+// ogni 5 minuti leggi la temperatura e lo stato della caldaia
+// memorizza i dati sul DB
+// grafica lo storico orario
+// richiedi in input la temperatura desiderata
+// se Tdes > Tact then turnon caldaia
+// loop
