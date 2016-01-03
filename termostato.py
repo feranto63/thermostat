b5375ea84051cdd63fe89f22525add72d29004e9
Ferruccio Antonelli <feranto63@gmail.com>
2016-01-03 15:50:25 +0100

    Update termostato.py

--- termostato.py
+++ termostato.py
@@ -4,8 +4,8 @@
 #memorizza i dati sul DB
 #grafica lo storico orario
 #richiedi in input la temperatura desiderata
-Tdes=raw_input("temperatura desiderata = ")
-print "Tdes=",Tdes
+#Tdes=raw_input("temperatura desiderata = ")
+#print "Tdes=",Tdes
 #se Tdes > Tact then turnon caldaia
 #loop
 
