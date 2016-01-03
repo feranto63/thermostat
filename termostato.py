d42aa1598bc3a6adc55085f4595bb7bb1814d5eb
Ferruccio Antonelli <feranto63@gmail.com>
2016-01-04 00:08:22 +0100

    Update termostato.py

--- termostato.py
+++ termostato.py
@@ -31,8 +31,8 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+###device_folder = glob.glob(base_dir + '28*')[0]
+###device_file = device_folder + '/w1_slave'
 
 def read_temp_raw():
     f = open(device_file, 'r')
