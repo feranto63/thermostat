f5ac3181c23bdb055a6df84b9724f7e7fdabc8ea
Ferruccio Antonelli <feranto63@gmail.com>
2016-01-26 18:48:07 +0100

    Update testReadEmail.py

--- testReadEmail.py
+++ testReadEmail.py
@@ -33,19 +33,19 @@ def read_gmail():
     (retcode, messages) = mail.search(None, '(UNSEEN)')
     if retcode == 'OK':
 
-    for num in messages[0].split() :
-      print 'Processing '
-      n=n+1
-      typ, data = mail.fetch(num,'(RFC822)')
-      for response_part in data:
-         if isinstance(response_part, tuple):
-             original = email.message_from_string(response_part[1])
+        for num in messages[0].split() :
+            print 'Processing '
+            n=n+1
+            typ, data = mail.fetch(num,'(RFC822)')
+            for response_part in data:
+                if isinstance(response_part, tuple):
+                    original = email.message_from_string(response_part[1])
 
-             print original['From']
-             print original['Subject']
-             typ, data = mail.store(num,'+FLAGS','\\Seen')
+                    print original['From']
+                    print original['Subject']
+                    typ, data = mail.store(num,'+FLAGS','\\Seen')
 
-    print n
+        print n
     
 # get most recent email id
 #    if id_list:
