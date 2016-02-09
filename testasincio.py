842e0d5a3c45ff3be2e79b554a53b8ffd02d479a
Ferruccio Antonelli <feranto63@gmail.com>
2016-02-09 21:28:13 +0100

    Create testasincio.py
    to test asyncio

---
+++ testasincio.py
@@ -0,0 +1,83 @@
+import sys
+import asyncio
+import telepot
+import telepot.async
+
+"""
+$ python3.4 skeletona_extend.py <token>
+A skeleton for your **async** telepot programs.
+"""
+#import library for logging
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+############ legge da file il token del Telegram Bot e della chat id
+
+tokenpath = os.path.dirname(os.path.realpath(__file__)) + "/token2"
+chatidpath = os.path.dirname(os.path.realpath(__file__)) + "/chatid2"
+
+
+try:
+    tokenFile = open(tokenpath,'r')
+    TOKEN = tokenFile.read().strip()
+    tokenFile.close()
+except IOError: 
+    logging.error("Non ho trovato il file di token. E' necessario creare un file 'token' con la token telegram per il bot. In ogni caso questo file NON deve essere tracciato da git - viene ignorato perche' menzionato nel .gitignore.")
+    exit()
+
+logging.info("caricata token.")
+        
+try:
+    chatidFile = open(chatidpath,'r')
+    CHAT_ID = chatidFile.read().strip()
+    chatidFile.close()
+except IOError:
+    logging.error("Non ho trovato il file di chatId. E' necessario creare un file 'chatid' con la chatid telegram per il bot")
+    # In ogni caso questo file NON deve essere tracciato da git - viene ignorato perche' menzionato nel .gitignore.")
+
+logging.info("caricata chatId.")
+
+class YourBot(telepot.async.Bot):
+    @asyncio.coroutine
+    def handle(self, msg):
+        flavor = telepot.flavor(msg)
+
+        # normal message
+        if flavor == 'normal':
+            content_type, chat_type, chat_id = telepot.glance(msg)
+            print('Normal Message:', content_type, chat_type, chat_id)
+
+            # Do your stuff according to `content_type` ...
+
+        # inline query - need `/setinline`
+        elif flavor == 'inline_query':
+            query_id, from_id, query_string = telepot.glance(msg, flavor=flavor)
+            print('Inline Query:', query_id, from_id, query_string)
+
+            # Compose your own answers
+            articles = [{'type': 'article',
+                            'id': 'abc', 'title': 'ABC', 'message_text': 'Good morning'}]
+
+            yield from bot.answerInlineQuery(query_id, articles)
+
+        # chosen inline result - need `/setinlinefeedback`
+        elif flavor == 'chosen_inline_result':
+            result_id, from_id, query_string = telepot.glance(msg, flavor=flavor)
+            print('Chosen Inline Result:', result_id, from_id, query_string)
+
+            # Remember the chosen answer to do better next time
+
+        else:
+            raise telepot.BadFlavor(msg)
+
+
+TOKEN = sys.argv[1]  # get token from command-line
+
+bot = YourBot(TOKEN)
+loop = asyncio.get_event_loop()
+
+loop.create_task(bot.messageLoop())
+print('Listening ...')
+
+loop.run_forever()
