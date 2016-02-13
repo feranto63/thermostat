424f13129e0c9291e22aa5f16ea5697b4666f87d
Ferruccio Antonelli <feranto63@gmail.com>
2016-02-13 22:52:42 +0100

    Update testschedule.py

--- testschedule.py
+++ testschedule.py
@@ -12,13 +12,17 @@ mySchedule=[["lun",17,17,17,17,17,17,20,20,20,18,18,18,18,20,20,18,18,18,18,18,2
             ["dom",17,17,17,17,17,17,17,17,20,20,18,18,18,20,20,20,18,18,18,18,20,20,20,17]]
  
  
-orario = time.localtime(time.time())
+#orario = time.localtime(time.time())
+now = time.time()
+orario = time.localtime(now)
+   
+
 curr_year=int(time.strftime("%Y",orario))
 curr_month=int(time.strftime("%m",orario)) 
 curr_day=int(time.strftime("%e",orario))
 curr_hour=int(time.strftime("%H",orario))
+
 localtime = time.asctime( orario )
 day_of_week= calendar.weekday(curr_year,curr_month,curr_day)
 
-print "localtime:",localtime, " day_of_week:", day_of_week, " curr_hour:",curr_hour," temp target:"
-#, mySchedule[day_of_week,curr_hour+1] 
+print "localtime:",localtime, " day_of_week:", day_of_week, " curr_hour:",curr_hour," temp target:", mySchedule[day_of_week][curr_hour+1] 
