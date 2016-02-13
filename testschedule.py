f83295266a29b8763897247f95f65001300d9526
Ferruccio Antonelli <feranto63@gmail.com>
2016-02-13 22:45:59 +0100

    Update testschedule.py

--- testschedule.py
+++ testschedule.py
@@ -20,4 +20,5 @@ curr_hour=int(time.strftime("%H",orario))
 localtime = time.asctime( orario )
 day_of_week= calendar.weekday(curr_year,curr_month,curr_day)
 
-print "localtime:",localtime, " day_of_week:", day_of_week, " curr_hour:",curr_hour," temp target:", mySchedule[day_of_week,curr_hour+1] 
+print "localtime:",localtime, " day_of_week:", day_of_week, " curr_hour:",curr_hour," temp target:"
+#, mySchedule[day_of_week,curr_hour+1] 
