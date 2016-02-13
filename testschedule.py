da3d0651deef65aa0ee78923be3955ff40dcdd72
Ferruccio Antonelli <feranto63@gmail.com>
2016-02-13 22:31:30 +0100

    Create testschedule.py

---
+++ testschedule.py
@@ -0,0 +1,13 @@
+import time
+
+import myschedule.py
+
+orario = time.localtime(now)
+curr_year=num(strftime("%Y",orario))
+curr_month=num(strftime("%m",orario)) 
+curr_day=num(strftime("%e",orario))
+curr_hour=num(strftime("%H",orario))
+localtime = time.asctime( orario )
+day_of_week= calendar.weekday(curr_year,curr_month,curr_day)
+
+print "localtime:",localtime, " day_of_week:", day_of_week, " curr_hour:",curr_hour," temp target:", tempSchedule[day_of_week,curr_hour+1] 
