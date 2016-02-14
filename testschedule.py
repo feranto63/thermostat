462d666bd2af0832428c4c38a2deaab0f2427395
Ferruccio Antonelli <feranto63@gmail.com>
2016-02-14 21:20:25 +0100

    Update testschedule.py

--- testschedule.py
+++ testschedule.py
@@ -45,8 +45,8 @@ fileschedule.close()  #chiude il file dei dati e lo salva
 #fileschedule.close()  #chiude il file dei dati e lo salva
 fileschedule = open("fileschedule","r")  #apre il file dei dati in append mode, se il file non esiste lo crea
 for i in range (0,7):
-            for y in range (0,25):
-                        mySchedule[i][y]=fileschedule.read().split(",")  #scrive la info di presence ed il timestam sul file
+            #for y in range (0,25):
+                        mySchedule[i]=fileschedule.readline().split(",")  #scrive la info di presence ed il timestam sul file
 fileschedule.close()  #chiude il file dei dati e lo salva
 
 print mySchedule
