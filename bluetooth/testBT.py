523545c1895b3da11bf45dd75cde072aea99abbd
Ferruccio Antonelli <feranto63@gmail.com>
2016-03-08 23:40:45 +0100

    Update testBT.py

--- bluetooth/testBT.py
+++ bluetooth/testBT.py
@@ -12,19 +12,15 @@ while True:
   result = bluetooth.lookup_name('F0:5B:7B:43:42:68', timeout=5)
   if (result != None):
     print "Fer: in"
-  if FerIN == 0:
-    FerIN = FerIN + 1
+  #if FerIN == 0:
+   # FerIN = FerIN + 1
     #import subprocess
     #subprocess.call(['bash','/home/pi/alarma_apaga.sh'])
     #subprocess.call(['bash','/home/pi/textoAvoz.sh','Bienvenido a casa Hector'])
-    FerOUT = 0
-    time.sleep(3)
+    #FerOUT = 0
+    time.sleep(10)
   else:
     print "Fer: out"
-    FerIN = 0
-    FerOUT = FerOUT + 1
-  print "Fer IN:"
-  print FerIN
-  print "FerOUT"
-  print FerOUT
+    #FerIN = 0
+    #FerOUT = FerOUT + 1
   print "------"
