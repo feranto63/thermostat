ee4627373c690444e316e1e819cc67207ef76d93
Ferruccio Antonelli <feranto63@gmail.com>
2016-03-28 22:57:50 +0200

    Update thermogram2.py

--- thermogram2.py
+++ thermogram2.py
@@ -303,6 +303,35 @@ def read_temp():
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c #, temp_f
 
+################## lettura del sensore DHT 11 temperatura e umidita'
+import Adafruit_DHT
+
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+def read_TandH():
+    sensor = Adafruit_DHT.DHT11
+    # Example using a Raspberry Pi with DHT sensor
+    # connected to GPIO4.
+    pin = 4
+    
+    # Try to grab a sensor reading.  Use the read_retry method which will retry up
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    
+    # Note that sometimes you won't get a reading and
+    # the results will be null (because Linux can't
+    # guarantee the timing of calls to read the sensor).
+    # If this happens try again!
+    if humidity is not None and temperature is not None:
+        print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+        return (temperature, humidity)
+    else:
+        print 'Failed to get reading. Try again!'
+        return (None, None)
+
+########## fine gestione sensore DHT11 ############################
+
+
 ##################### funzione per la gestione dei messaggi di presence
 def set_presence(presence_msg):
     global persona_at_home, who_is_at_home, how_many_at_home
