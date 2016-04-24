2bfb8a6cd12d9b56af0657637fe872e485da3ace
Ferruccio Antonelli <feranto63@gmail.com>
2016-04-24 23:29:03 +0200

    Create testreadsql.py

---
+++ testreadsql.py
@@ -0,0 +1,22 @@
+import sqlite3
+import threading
+import os
+import glob
+import time
+
+#Sqlite Database where to store readings
+dbname='/var/www/templog.db'
+
+conn=sqlite3.connect(dbname)
+
+selectQuery = "SELECT  * FROM  sqlite_sequence WHERE name = temps";
+Cursor cursor = db.rawQuery(selectQuery, null);
+cursor.moveToLast();curs=conn.cursor()
+#dati_da_inserire = [orario,temp,tempDHT,humidity]
+#curs.execute("INSERT INTO temps values (?,?,?,?)", dati_da_inserire)
+rows = cursor.fetchone()
+# commit the changes
+conn.commit()
+conn.close()
+
+print rows
