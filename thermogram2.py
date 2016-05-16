1234222661440aad5405da342c4b8344d459203a
Ferruccio Antonelli <feranto63@gmail.com>
2016-05-16 22:50:25 +0200

    Update thermogram2.py

--- thermogram2.py
+++ thermogram2.py
@@ -370,8 +370,9 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+if DS_PRESENCE:
+    device_folder = glob.glob(base_dir + '28*')[0]
+    device_file = device_folder + '/w1_slave'
 
 def read_temp_raw():
     f = open(device_file, 'r')
