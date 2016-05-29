6ed6bbdf0ea73d85b850f1e5014ea906b8df24c9
Ferruccio Antonelli <feranto63@gmail.com>
2016-05-29 11:28:56 +0200

    Update thermotest.py

--- thermotest.py
+++ thermotest.py
@@ -69,85 +69,8 @@ import logging
 logging.basicConfig(filename='termostato.log', level=logging.WARNING)
 
 
-###################### database per la memorizzazione delle temperature ###############
-import sqlite3
-
-# store the temperature in the database
-def log_temperature(orario,temp, tempDHT, humidity, ExtTemp, HeatOn, TargetTemp):
-
-    conn=sqlite3.connect(dbname)
-    curs=conn.cursor()
-#    curs.execute("INSERT INTO temps values(datetime('now'), (?))", (temp,))
-    dati_da_inserire = [orario,temp,tempDHT,humidity, ExtTemp, HeatOn, TargetTemp]
-    curs.execute("INSERT INTO temps values (?,?,?,?,?,?,?)", dati_da_inserire)
-    # commit the changes
-    conn.commit()
-    conn.close()
-
-################### gestione cronotermostato ###########################
-import calendar
-
-#import thermoschedule
-# schedulazione della programmazione della temperatura
-#mySchedule is a matrix [7 x 24] [lunedi' is first row]
-mySchedule = [['17' for x in range(24)] for x in range(7)] 
-
-def initialize_schedule():
-    global mySchedule, FILESCHEDULE
-    try:
-        fileschedule = open(FILESCHEDULE,"r")  #apre il file dei dati in append mode, se il file non esiste lo crea
-        for i in range (0,7):
-            tmpstr=fileschedule.readline().strip(";\n")
-            mySchedule[i]=tmpstr.split(";")  #scrive la info di presence ed il timestam sul file
-        fileschedule.close()  #chiude il file dei dati e lo salva
-    except IOError:
-        mySchedule= [['17','17','17','17','17','17','20','20','20','18','18','18','18','20','20','18','18','18','18','18','20','20','20','17'],
-                    ['17','17','17','17','17','17','20','20','20','18','18','18','18','20','20','18','18','18','18','18','20','20','20','17'],
-                    ['17','17','17','17','17','17','20','20','20','18','18','18','18','20','20','18','18','18','18','18','20','20','20','17'],
-                    ['17','17','17','17','17','17','20','20','20','18','18','18','18','20','20','18','18','18','18','18','20','20','20','17'],
-                    ['17','17','17','17','17','17','20','20','20','18','18','18','18','20','20','18','18','18','18','18','20','20','20','17'],
-                    ['17','17','17','17','17','17','20','20','20','18','18','18','18','20','20','18','18','18','18','18','20','20','20','17'],
-                    ['17','17','17','17','17','17','17','17','20','20','18','18','18','20','20','20','18','18','18','18','20','20','20','17'],
-                    ['17','17','17','17','17','17','17','17','20','20','18','18','18','20','20','20','18','18','18','18','20','20','20','17']]
-
-def current_target_temp():
-    global mySchedule
-    now = time.time()
-    orario = time.localtime(now)
-   
-    curr_year=int(time.strftime("%Y",orario))
-    curr_month=int(time.strftime("%m",orario)) 
-    curr_day=int(time.strftime("%e",orario))
-    curr_hour=int(time.strftime("%H",orario))
-
-    localtime = time.asctime( orario )
-    day_of_week= calendar.weekday(curr_year,curr_month,curr_day)
-
-    target_temp=mySchedule[day_of_week][curr_hour]
-    return(float(target_temp))
-
-def save_schedule():
-    global mySchedule, FILESCHEDULE
-    
-    fileschedule = open(FILESCHEDULE,"w")  #apre il file dei dati in append mode, se il file non esiste lo crea
-    for i in range (0,7):
-        for y in range (0,24):
-            fileschedule.write(mySchedule[i][y]+";")
-        fileschedule.write("\n")#scrive la info di presence ed il timestam sul file
-    fileschedule.close()  #chiude il file dei dati e lo salva
-
-################### fine gestione cronotermostato ######################
-
-def isnumeric(s):
-    try:
-        i = float(s)
-        return True
-    except (ValueError, TypeError):
-        return False
 
 
-# definisce la variabile per la conferma dell'apertura del cancello
-opengate_confirming = False
 
 ###################### gestisce i comandi inviati al Telegram Bot
 def handle(msg):
@@ -304,23 +227,8 @@ def handle(msg):
 
 
 
-############ legge da file il token del Telegram Bot e della chat id
+TOKEN = '186703015:AAEJ2qKIPwbhktlPF0qdfLWHFJFSlE3uhBc'
 
-tokenpath = os.path.dirname(os.path.realpath(__file__)) + "/token"
-chatidpath = os.path.dirname(os.path.realpath(__file__)) + "/chatid"
-chatidgatepath = os.path.dirname(os.path.realpath(__file__)) + "/chatid_cancello"
-
-
-try:
-    tokenFile = open(tokenpath,'r')
-    TOKEN = tokenFile.read().strip()
-    tokenFile.close()
-except IOError: 
-    logging.error("Non ho trovato il file di token. E' necessario creare un file 'token' con la token telegram per il bot. In ogni caso questo file NON deve essere tracciato da git - viene ignorato perche' menzionato nel .gitignore.")
-    exit()
-
-logging.info("caricata token.")
-        
 try:
     chatidFile = open(chatidpath,'r')
     CHAT_ID = chatidFile.read().strip()
@@ -331,125 +239,6 @@ except IOError:
 
 logging.info("caricata chatId.")
 
-if GATE_PRESENT:
-    try:
-        chatidFile = open(chatidgatepath,'r')
-        CHAT_ID_GATE = chatidFile.read().strip()
-        chatidFile.close()
-    except IOError:
-        CHAT_ID_GATE = CHAT_ID #se non c'e' il cancello metto lo stesso chat_id principale
-        logging.error("Non ho trovato il file di chatId_cancello. E' necessario creare un file 'chatid' con la chatid telegram per il bot")
-        sys.exit(9)
-        # In ogni caso questo file NON deve essere tracciato da git - viene ignorato perche' menzionato nel .gitignore.")
-
-    logging.info("caricata chatIdGate.")
-
-
-# variables for periodic reporting
-last_report = time.time()
-report_interval = 300  # report every 300 seconds (5 min) as a default
-
-# variable for heating status
-heating_status = False
-heating_standby = False
-heating_overwrite = False
-
-CurTempDHT = 0
-CurHumidity = 0
-
-# variables for cleaning period
-pulizie_status=False
-pulizie_timer=None
-
-
-################# gestione della interfaccia di GPIO   
-# wiringpi numbers  
-import RPi.GPIO as GPIO
-##import wiringpi2 as wiringpi
-##wiringpi.wiringPiSetup()
-##wiringpi.pinMode(0, 1) # sets WP pin 0 to output
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(HEAT_PIN,GPIO.OUT)
-GPIO.setup(GATE_PIN,GPIO.OUT)
-
-#Find temperature from thermometer
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-
-base_dir = '/sys/bus/w1/devices/'
-if DS_PRESENCE:
-    device_folder = glob.glob(base_dir + '28*')[0]
-    device_file = device_folder + '/w1_slave'
-
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c #, temp_f
-
-################## lettura del sensore DHT 11 temperatura e umidita'
-import Adafruit_DHT
-
-# Sensor should be set to Adafruit_DHT.DHT11,
-# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
-def read_TandH():
-    global DHT_PIN
-    sensor = Adafruit_DHT.DHT22
-    # Example using a Raspberry Pi with DHT sensor
-    # connected to GPIO4.
-    pin = DHT_PIN
-    
-    # Try to grab a sensor reading.  Use the read_retry method which will retry up
-    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    
-    # Note that sometimes you won't get a reading and
-    # the results will be null (because Linux can't
-    # guarantee the timing of calls to read the sensor).
-    # If this happens try again!
-    if humidity is not None and temperature is not None:
-        print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
-        return (temperature, humidity)
-    else:
-        print 'Failed to get reading. Try again!'
-        return (None, None)
-
-########## fine gestione sensore DHT11 ############################
-
-########## gestione sensori Radio CISECO ###############
-import serial
-
-
-def get_temp_radio():
-    DEVICE = '/dev/ttyAMA0'
-    BAUD = 9600
-
-    ser = serial.Serial(DEVICE, BAUD)
-    n = ser.inWaiting()
-    if n != 0:
-        msg = ser.read(n)
-        deviceID = msg[1:3]
-        messType = msg[3:7]
-        value = msg[7:12]
-    else:
-        deviceID = "--"
-        messType = "NULL"
-        value = 0
-    return deviceID, messType, value
-    
-################### fine gestione sensori Radio CISECO ##############
 
 
 ##################### funzione per la gestione dei messaggi di presence
@@ -572,26 +361,6 @@ def check_presence_IP():
 
 ####################################################
 
-############# controlla la presence con ping BT #################        
-def check_presence_BT():
-    global persona_BT, persona_at_home, persone_della_casa
-    global CHAT_ID
-    for n in range (persone_della_casa):
-        result = bluetooth.lookup_name(persona_BT[n], timeout=5)
-        if (result != None):
-            if not persona_at_home[n]:
-                changed, messaggio_IN_OUT= set_presence(n, persona[n]+' IN') #richiama la funzione per la gestisce della presence
- #               if changed:
- #                   bot.sendMessage(CHAT_ID, messaggio_IN_OUT)
-
-        else:
-            if persona_at_home[n]:
-                changed, messaggio_IN_OUT= set_presence(n, persona[n]+' OUT') #richiama la funzione per la gestisce della presence
- #               if changed:
- #                   bot.sendMessage(CHAT_ID, messaggio_IN_OUT)
-
-###################################################
-
 
 ##### connette o riconnette alla mail ###########
 def connect(retries=5, delay=3):
@@ -609,49 +378,9 @@ def connect(retries=5, delay=3):
                 raise
 #################################################
 
-############## gestione del riscaldamento ##################
-def TurnOnHeating():
-    global heating_status, heating_standby, heating_overwrite, FILEHEATING, CHAT_ID
-    global HEAT_PIN, HEAT_ON
-
-    heating_status = True #print "HEATING ON "+localtime+"\n"
-    f = open("heating_status","w")
-    f.write('ON')
-    f.close()  #chiude il file dei dati e lo salva
+   
     
-    if not heating_overwrite and heating_standby:
-       bot.sendMessage(CHAT_ID, "Fa un po' freddo, Padrone, ma solo solo a casa e faccio un po' di economia")
-    else:
-        GPIO.output(HEAT_PIN, HEAT_ON) # sets port 0 to 1 (3.3V, on) per accendere i termosifoni
-        bot.sendMessage(CHAT_ID, "Accendo il riscaldamento, Padrone")
-        orario = time.localtime(time.time())
-        localtime = time.asctime( orario )
-        ora_minuti = time.strftime("%H:%M", orario)
-        fileheating = open(FILEHEATING,"a")  #apre il file dei dati in append mode, se il file non esiste lo crea
-        fileheating.write("ON,"+localtime+"\n")  #scrive la info di accensione del riscaldamento il timestamp su file
-        fileheating.close()  #chiude il file dei dati e lo salva
-
-    
-    
-def TurnOffHeating():
-    global heating_status, heating_standby, heating_overwrite, FILEHEATING, CHAT_ID
-    global HEAT_PIN, HEAT_OFF
-    heating_status = False
-    f = open("heating_status","w")
-    f.write('OFF')
-    f.close()  #chiude il file dei dati e lo salva
-    
-    GPIO.output(HEAT_PIN, HEAT_OFF) # sets port 0 to 0 (0V, off) per spegnere i termosifoni
-    bot.sendMessage(CHAT_ID, "Spengo il riscaldamento, Padrone")
-    orario = time.localtime(time.time())
-    localtime = time.asctime( orario )
-    ora_minuti = time.strftime("%H:%M", orario)
-    fileheating = open(FILEHEATING,"a")  #apre il file dei dati in append mode, se il file non esiste lo crea
-    fileheating.write("OFF,"+localtime+"\n")  #scrive la info di accensione del riscaldamento il timestamp su file
-    fileheating.close()  #chiude il file dei dati e lo salva
-
-
-##################### inizio gestione presence via email ################
+#################### inizio gestione presence via email ################
 #connect to gmail
 def read_gmail():
     global mail
@@ -705,18 +434,6 @@ def is_connected():
     return False
 ######### fine test internet connection
 
-# -------------------- inizio programma
-######## Inizializza le temperature
-if DS_PRESENCE:
-    CurTemp = read_temp()
-else:
-    CurTemp = 99
-    
-if DHT_PRESENCE:
-    CurTempDHT, CurHumidity = read_TandH()
-else:
-    CurTempDHT = 99
-    CurHumidity = 'N.A.'
 
 ############ legge da file lo stato delle persone della casa ###############
 for n in range(persone_della_casa):
@@ -731,62 +448,17 @@ for n in range(persone_della_casa):
     except IOError:
         persona_at_home[n] = False  #se il file non e' presente imposto la presence a False
 
-######## legge da file lo stato del riscaldamento e dello standby ###########
-try:
-    f = open("heating_status","r")  #apre il file dei dati in read mode
-    h_status=f.read().strip()   #legge la info di presence sul file
-    f.close()  #chiude il file dei dati e lo salva
-    if h_status == "ON":
-        heating_status = True
-    else:
-        heating_status = False
-except IOError:
-    heating_status = False  #se il file non e' presente imposto la presence a False
-
-try:
-    f = open("heating_standby","r")  #apre il file dei dati in read mode
-    hby_status=f.read().strip()   #legge la info di presence sul file
-    f.close()  #chiude il file dei dati e lo salva
-    if hby_status == "ON":
-        heating_standby = True
-    else:
-        heating_standby = False
-except IOError:
-    heating_standby = False  #se il file non e' presente imposto la presence a False
-
-heating_overwrite = False
-
-######## legge da file lo programmazione del cronotermostato ###########
-initialize_schedule()
-
 ######## inizializza il bot Telegram ###########
 bot = telepot.Bot(TOKEN)
 bot.message_loop(handle)
 logging.info("Listening ...")
 
-help_or_gate = '/help'
-if GATE_PRESENT:
-    help_or_gate = '/apri'
 
 main_show_keyboard = {'keyboard': [['/now','/casa'], ['/ho_caldo','/ho_freddo'],['/pulizie',help_or_gate]]} #tastiera personalizzata
 bot.sendMessage(CHAT_ID, 'Mi sono appena svegliato, Padrone', disable_notification=debug_notify)
 
-if heating_status and not heating_standby:
-    TurnOnHeating()
-    #GPIO.output(HEAT_PIN, HEAT_ON) # sets port 0 to 0 (3.3V, off) per spengere i termosifoni
-    bot.sendMessage(CHAT_ID, "Rispristino il riscaldamento, Padrone", disable_notification=debug_notify)
-else:
-    TurnOffHeating()
-    #GPIO.output(HEAT_PIN, HEAT_OFF)
-
-GPIO.output(GATE_PIN, GATE_OFF) #pulisce il circuito di apertura cancello
 
 bot.sendMessage(CHAT_ID, 'Come ti posso aiutare?', reply_markup=main_show_keyboard, disable_notification=debug_notify)
-
-#predispone la tastiera per i visitatori della casa
-if GATE_PRESENT:
-    show_keyboard = {'keyboard': [['/apri']], 'resize_keyboard':True} #tastiera personalizzata
-    bot.sendMessage(CHAT_ID_GATE, "Premere /apri per aprire il cancello", reply_markup=show_keyboard, disable_notification=debug_notify)
 
 mail = connect() #apre la casella di posta
 
@@ -794,48 +466,6 @@ while True:
     now = time.time()
     #localtime = time.asctime( time.localtime(now) )
     localtime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-    CurTargetTemp=current_target_temp()
-    if DS_PRESENCE:
-        CurTemp = read_temp()
-    if DHT_PRESENCE:
-        if DS_PRESENCE == False:
-            CurTemp = CurTempDHT
-    if CurHumidity == None:
-        CurHumidity = 'N.A.'
-    if not heating_overwrite:
-        if pulizie_status:
-            if now >= pulizie_timer:
-                pulizie_status=False
-                bot.sendMessage(CHAT_ID, "E' terminato il periodo per le pulizie, Padrone", disable_notification=debug_notify)
-        else:
-            if not heating_status:
-                if CurTemp < (CurTargetTemp - 0.2):
-                    TurnOnHeating()
-            else:
-                if CurTemp > (CurTargetTemp + 0.2):
-                    TurnOffHeating()
-    if report_interval is not None and last_report is not None and now - last_report >= report_interval:
-        if DHT_PRESENCE:
-            CurTempDHT, CurHumidity = read_TandH()
-        else:
-            CurTempDHT = 99
-            CurHumidity = 'N.A.'
-#        deviceID, msgType, value = get_temp_radio()
-        #apre il file dei dati in append mode, se il file non esiste lo crea
-        filedati = open("filedati","a")
-        #scrive la temperatura coreente ed il timestam sul file
-        filedati.write("T="+str(CurTemp)+",HR="+str(CurHumidity)+"@"+localtime+"\n")
-#        if deviceID != '--':
-#            filedati.write("ID="+deviceID+",msgType="+msgType+",value="+str(value))
-        #chiude il file dei dati e lo salva
-        filedati.close()
-        log_temperature(localtime,CurTemp,CurTempDHT,CurHumidity, 0, heating_status, CurTargetTemp)
-#        log_temperature(orario,temp, tempDHT, humidity, ExtTemp, HeatOn, TargetTemp)
-        last_report = now
-    # verifica se ci sono nuovi aggiornamenti sulla presence (via email)
-#    if is_connected():
-#        read_gmail()
-            
     #check_presence_BT()
     if IP_PRESENCE:
         check_presence_IP() # controlla la presente con ping IP
