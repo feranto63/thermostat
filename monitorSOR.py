11e37a68c0df95db1cce6f9fb3939ed9807e6b5a
Ferruccio Antonelli <feranto63@gmail.com>
2016-07-26 22:23:42 +0200

    Update monitorSOR.py

--- monitorSOR.py
+++ monitorSOR.py
@@ -28,7 +28,7 @@ DHT_PIN = 4
 dbname='/var/www/templog.db'
 
 #Serial devices
-DEVICE = '/dev/ttyAMA0'
+DEVICE = '/dev/ttyS0'
 #DEVICE = '/dev/tty.Bluetooth-Incoming-Port'
 BAUD = 9600
 
@@ -105,70 +105,6 @@ def get_temp_radio():
 			print("Bat msg= "+str(tempvalue))
     return (deviceid,msgType,tempvalue)
 
-################# gestione della interfaccia di GPIO   
-# wiringpi numbers  
-import RPi.GPIO as GPIO
-##import wiringpi2 as wiringpi
-##wiringpi.wiringPiSetup()
-##wiringpi.pinMode(0, 1) # sets WP pin 0 to output
-GPIO.setmode(GPIO.BCM)
-#GPIO.setup(HEAT_PIN,GPIO.OUT)
-#GPIO.setup(GATE_PIN,GPIO.OUT)
-
-#Find temperature from thermometer
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
-
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c #, temp_f
-
-################## lettura del sensore DHT 11 temperatura e umidita'
-import Adafruit_DHT
-
-# Sensor should be set to Adafruit_DHT.DHT11,
-# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
-def read_TandH():
-    global DHT_PIN
-    sensor = Adafruit_DHT.DHT22
-    # Example using a Raspberry Pi with DHT sensor
-    # connected to GPIO4.
-    pin = DHT_PIN
-    
-    # Try to grab a sensor reading.  Use the read_retry method which will retry up
-    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    
-    # Note that sometimes you won't get a reading and
-    # the results will be null (because Linux can't
-    # guarantee the timing of calls to read the sensor).
-    # If this happens try again!
-    if humidity is not None and temperature is not None:
-        print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
-        return (temperature, humidity)
-    else:
-        print 'Failed to get reading. Try again!'
-        return (None, None)
-
-########## fine gestione sensore DHT11 ############################
 
 ########## gestione sensori Radio CISECO ###############
 #import serial
