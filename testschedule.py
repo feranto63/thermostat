7856d9ba15c8dccb6320ee5a12ae457977f9725d
Ferruccio Antonelli <feranto63@gmail.com>
2016-10-18 21:50:51 +0200

    Update testschedule.py

--- testschedule.py
+++ testschedule.py
@@ -39,7 +39,8 @@ def put_tempschedule(day,time,temp):
    
    conn=sqlite3.connect(dbname)
    curs=conn.cursor()
-   curs.execute("UPDATE tempschedule SET "+column_name+" VALUE ? WHERE giorno == ?", (temp, day_index)) 
+   command="UPDATE tempschedule SET "+column_name+" VALUE ? WHERE giorno = ?"
+   curs.execute(command, (temp, day_index)) 
 #   curs.execute("SELECT * FROM tempschedule")
 #   for i in range (7):
 #      data=curs.fetchone()
