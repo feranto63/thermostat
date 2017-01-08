5499e0bbeeecec4e3b591aa024fa6b82f6d67c37
Ferruccio Antonelli <feranto63@gmail.com>
2017-01-08 11:20:27 +0100

    Update rf24receiver.cpp

--- rf24receiver.cpp
+++ rf24receiver.cpp
@@ -102,10 +102,10 @@ CREATE TABLE w_temps (timestamp DATETIME, sensor_id NUMERIC, temp NUMERIC, humid
 */
 				time_t curtime;
 				time(&curtime);
-/*				printf("Current time = %s", ctime(&curtime));*/
 				t_stamp = ctime(&curtime);
+				printf("Current time = %s", t_stamp);
 
-				sql = "INSERT INTO w_temps (%s,%i, %f, %f);", t_stamp, header.from_node, message.temperature,message.humidity;
+				sprintf(sql,"INSERT INTO w_temps (%s, %i, %f, %f);", t_stamp, header.from_node, message.temperature,message.humidity;
 				printf(sql);
    				/* Execute SQL statement */
    				rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
