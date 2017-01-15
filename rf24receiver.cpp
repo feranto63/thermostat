77e7615272e40fe23205c86ea03492c477b65d6f
Ferruccio Antonelli <feranto63@gmail.com>
2017-01-15 17:39:48 +0100

    Update rf24receiver.cpp

--- rf24receiver.cpp
+++ rf24receiver.cpp
@@ -43,6 +43,116 @@ void log_w_sensor (sqlite3 *db, char *t_stamp, int node_id, float temp, float hu
    	}
 }
 
+
+// Structure of our message
+struct message_t {
+  float temperature;
+  float humidity;
+};
+
+
+/* dht sensor reading values
+*/
+
+/*
+ *  dht.c:
+ *	read temperature and humidity from DHT11 or DHT22 sensor
+ */
+
+#include <wiringPi.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+#include <stdint.h>
+
+#define MAX_TIMINGS	85
+#define DHT_PIN		1	/* GPIO-18 */
+
+int data[5] = { 0, 0, 0, 0, 0 };
+
+message_t read_dht_data()
+{
+	uint8_t laststate	= HIGH;
+	uint8_t counter		= 0;
+	uint8_t j		= 0, i;
+	message_t result;
+
+	data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+
+	/* pull pin down for 18 milliseconds */
+	pinMode( DHT_PIN, OUTPUT );
+	digitalWrite( DHT_PIN, LOW );
+	delay( 18 );
+
+	/* prepare to read the pin */
+	pinMode( DHT_PIN, INPUT );
+
+	/* detect change and read data */
+	for ( i = 0; i < MAX_TIMINGS; i++ )
+	{
+		counter = 0;
+		while ( digitalRead( DHT_PIN ) == laststate )
+		{
+			counter++;
+			delayMicroseconds( 1 );
+			if ( counter == 255 )
+			{
+				break;
+			}
+		}
+		laststate = digitalRead( DHT_PIN );
+
+		if ( counter == 255 )
+			break;
+
+		/* ignore first 3 transitions */
+		if ( (i >= 4) && (i % 2 == 0) )
+		{
+			/* shove each bit into the storage bytes */
+			data[j / 8] <<= 1;
+			if ( counter > 16 )
+				data[j / 8] |= 1;
+			j++;
+		}
+	}
+
+	/*
+	 * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+	 * print it out if data is good
+	 */
+	if ( (j >= 40) &&
+	     (data[4] == ( (data[0] + data[1] + data[2] + data[3]) & 0xFF) ) )
+	{
+		float h = (float)((data[0] << 8) + data[1]) / 10;
+		if ( h > 100 )
+		{
+			h = data[0];	// for DHT11
+		}
+		float c = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
+		if ( c > 125 )
+		{
+			c = data[2];	// for DHT11
+		}
+		if ( data[2] & 0x80 )
+		{
+			c = -c;
+		}
+		float f = c * 1.8f + 32;
+		printf( "Humidity = %.1f %% Temperature = %.1f *C (%.1f *F)\n", h, c, f );
+		result.temperature = c;
+		result.humidity = h;
+	}else  {
+		printf( "Data not good, skip\n" );
+		result.temperature = NULL;
+		result.humidity = NULL;
+	}
+	return result;
+}
+
+
+//----- END OF DHT PROCEDURE
+
+
+
 /**
  * g++ -L/usr/lib main.cc -I/usr/include -o main -lrrd
  **/
@@ -60,11 +170,6 @@ const unsigned long interval = 2000;
 const int SAMPLE = 600; // intervallo in secondi per la memorizzazione delle temp nel db 10 minuti 10*60
 const int NUM_SENSORI = 2;
 
-// Structure of our message
-struct message_t {
-  float temperature;
-  float humidity;
-};
 
 int main(int argc, char** argv)
 {
