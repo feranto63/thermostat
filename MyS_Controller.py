a3e6b4220068556bf43ea9868c70d7d6cbc03615
Ferruccio Antonelli <feranto63@gmail.com>
2017-07-12 19:38:47 +0200

    Update MyS_Controller.py

--- MyS_Controller.py
+++ MyS_Controller.py
@@ -50,11 +50,12 @@ import imaplib
 import email
 
 #import library for logging
-import logging
+"""import logging
 logging.basicConfig(
         filename='/home/pi/BotMysController.log',
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.WARN)
+"""
 
 sensor = [['2017-06-15 22:02:27',25.3, 70.9] for x in range (20)]
 
