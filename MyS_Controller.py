d5bb7dd9913f16034c5fd638db904c9e9c5cc682
Ferruccio Antonelli <feranto63@gmail.com>
2017-10-23 23:06:40 +0200

    Update MyS_Controller.py

--- MyS_Controller.py
+++ MyS_Controller.py
@@ -253,6 +253,8 @@ except IOError:
 bot.sendMessage(CHATID,"Sono "+MaggiordomoID+". Inizio il monitoraggio dell'antifurto", disable_notification=True)
 
 HEAT_STATUS = read_heating_status()
+print("initial heat status"+HEAT_STATUS)
+
 
 #restore acensione riscaldamento
 if HEAT_STATUS:
