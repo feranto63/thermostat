21388a9568fec82be0d0327be96f6b1c9fde71f1
Ferruccio Antonelli <feranto63@gmail.com>
2017-12-26 12:59:27 +0100

    Update MyS_Controller.py

--- MyS_Controller.py
+++ MyS_Controller.py
@@ -387,7 +387,7 @@ while True:
         else:
             TurnOFF_termosifoni(HEAT_ID)
         HEAT_STATUS = CURRENT_HEAT
-    values = GATEWAY.sensors[heatID].children[1].values
+    values = GATEWAY.sensors[HEAT_ID].children[1].values
     print(values)
 
     # print("battery level="+ str(GATEWAY.sensors[34].battery_level))
