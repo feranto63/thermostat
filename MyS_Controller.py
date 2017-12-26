9a0de99a8722bbeefe4cfc61991dbd6c6021a0ff
Ferruccio Antonelli <feranto63@gmail.com>
2017-12-26 13:04:29 +0100

    Update MyS_Controller.py

--- MyS_Controller.py
+++ MyS_Controller.py
@@ -219,6 +219,7 @@ def TurnON_termosifoni(heatID):
     retries = 4
     while retries > 0:
         GATEWAY.set_child_value(heatID, 1, 2, 0)
+        time.sleep(15)
         values = GATEWAY.sensors[heatID].children[1].values[2]
         print(values)
         if values == 0:
@@ -242,6 +243,7 @@ def TurnOFF_termosifoni(heatID):
     while retries > 0:
 
         GATEWAY.set_child_value(heatID, 1, 2, 1)
+        time.sleep(15)
         values = GATEWAY.sensors[heatID].children[1].values[2]
         print(values)
         if values == 1:
