c5efe6cec252a63e65ebb48d531986dd5b615e91
Ferruccio Antonelli <f.antonelli@tin.it>
2018-07-22 18:14:25 +0200

    Update thermogram2.py

--- thermogram2.py
+++ thermogram2.py
@@ -397,10 +397,10 @@ def handle(msg):
 
 
     elif command == '/set':
-	if len(command_list) in [3,4]:
+    if len(command_list) in [3,4]:
             if (len(command_list) == 3):
                 time_to_set = int(command_list[1])
-	        temp_to_set = float(command_list[2])
+            temp_to_set = float(command_list[2])
                 days_to_set = range(7)
             else:
                 days_to_set = [ parse_weekday(command_list[1]) , ]
