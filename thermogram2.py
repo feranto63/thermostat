47195cd80082aefd4cf1a11f96336059688e473d
Ferruccio Antonelli <f.antonelli@tin.it>
2019-01-13 14:49:53 +0100

    Update thermogram2.py

--- thermogram2.py
+++ thermogram2.py
@@ -109,8 +109,8 @@ overwrite_temp = 25 #temperatura in gradi di funzionamento in overwrite da setta
 
 
 #MAIN_HEAT = [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # indica se usare la caldaia principale nell'ora x
-#MAIN_HEAT = [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]  # indica se usare la caldaia principale nell'ora x
-MAIN_HEAT  = [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  # indica se usare la caldaia principale nell'ora x
+MAIN_HEAT  = [1,1,1,1,1,1,1,1,1,1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]  # indica se usare la caldaia principale nell'ora x
+#MAIN_HEAT = [1,1,1,1,1,1,1,1,1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  # indica se usare la caldaia principale nell'ora x
 #            [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 FORCE_HOUR = [1,1,1,1,1,1,1,1,1,1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]  # indica se forzare la presenza
 
